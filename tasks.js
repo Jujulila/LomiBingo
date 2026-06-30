6162c4c118dfd63a2f79933d256b7d0c0fe445b9
Jujulila <95723450+Jujulila@users.noreply.github.com>
2026-06-30 17:37:46 +0200

    Add files via upload

---
+++ tasks.js
@@ -0,0 +1,18 @@
+const tasks = [
+  "Courir", 
+  "Dormir", 
+  "Prendre une photo du lever de soleil", 
+  "S'abonner a lomi",
+  "Manger sain", 
+  "Lacher son portable 24H", 
+  "Gagner à valo", 
+  "Boire de leau",
+  "Faire le trotoir", 
+  "Trouver un lapin doré", 
+  "Ouvrir la chambre des secrets", 
+  "Mettre l'anneau dans le volcans",
+  "Faire un marathon sur mars", 
+  "Serer la main de macron", 
+  "Mettre un stekette à cartlito", 
+  "Jouer du rock"
+];
